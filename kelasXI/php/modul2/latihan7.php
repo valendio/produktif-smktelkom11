c0e20d70cd852191aa313081c6da4499d55f7cdc
Mr.Saront0 <oryzavalendio@gmail.com>
2022-08-24 07:42:23 +0700

    array

---
+++ kelasXI/php/modul2/latihan7.php
@@ -0,0 +1,18 @@
+<?php
+
+$arrBuah = array("Mangga", "Apel", "Pisang", "Jeruk");
+echo $arrBuah[0];
+echo "<br>";
+echo $arrBuah[3];
+echo "<br>";
+
+$arrWarna = array();
+$arrWarna[] = "Merah";
+$arrWarna[] = "Hijau";
+$arrWarna[] = "Biru";
+$arrWarna[] = "Putih";
+
+echo $arrWarna[0];
+echo "<br>";
+echo $arrWarna[2];
+echo "<br>";
