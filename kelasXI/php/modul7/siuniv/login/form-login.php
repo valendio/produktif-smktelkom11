b07cfc638716fbebe2df5b7e5bded27595b1f09f
Mr.Saront0 <oryzavalendio@gmail.com>
2022-09-14 21:58:45 +0700

    add login

---
+++ kelasXI/php/modul7/siuniv/login/form-login.php
@@ -0,0 +1,32 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Login</title>
+</head>
+<body>
+
+<h1>Login</h1>
+
+<form action="login.php">
+    <table>
+        <tr>
+            <td>Username</td> <td>:</td>
+            <td><input type="text"></td>
+        </tr>
+        <tr>
+            <td>Password</td> <td>:</td>
+            <td><input type="text"></td>
+        </tr>
+    </table>
+</form>
+
+
+    
+</body>
+</html>
